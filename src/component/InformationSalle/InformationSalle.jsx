e779d18e39d153bad67e7014e483ab50d0cc54c4
WizzC <clementcarlier310@gmail.com>
2024-01-23 12:11:23 +0100

    AjoutConcertFin

--- src/component/InformationSalle/InformationSalle.jsx
+++ src/component/InformationSalle/InformationSalle.jsx
@@ -3,15 +3,15 @@ import Style from './InformationSalle.module.css';
 function InformationSalle({salle}) {
 
     const adresse = salle.adresse;
-
-    
-    const styles = salle.styles;
-
+    let styles = '';
+    if(salle.styles != null) {
+        styles = salle.styles.join('-');
+    }
     return (
         <div className={Style.info}>
         <p>{adresse.numero + " " + adresse.voie}</p>
         <p>{adresse.codePostal + " " + adresse.ville}</p>
-        <p>{styles.map((style) => style + " - "  )}</p>
+        <p>{styles}</p>
         <p>{"capacite " + salle.capacite}</p>
         {salle.smac != false && <p>smac</p>}
         </div>
