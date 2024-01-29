07e835f94a9138c36a8bf75499d46f2a0bed498e
WizzC <clementcarlier310@gmail.com>
2024-01-29 09:12:35 +0100

    eee

--- src/component/AfficherAvis/AfficherAvis.jsx
+++ src/component/AfficherAvis/AfficherAvis.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Rating } from 'react-simple-star-rating'
 import Styles from './AfficherAvis.module.css'
-
+import Avis from '../Avis/Avis.jsx'
 function AfficherAvis({ salle }) {
   const listeAvis = salle.avis;
   return (
@@ -12,28 +12,6 @@ function AfficherAvis({ salle }) {
 
   )
 }
-function Avis({ avis }) {
-
-  let date = new Date(avis.date);
-  return (
-    <div className={Styles.avisContaine}>
-      <p className={Styles.dateAvis}>{(date).toLocaleDateString("fr")}</p>
-      <Stars note={avis.note} />
-    </div>
-  )
-}
-const Stars = ({ note }) => {
-  let noteSurCinq = note / 2;
-  return (
-    <Rating
-      allowFraction
-      transition
-      initialValue={noteSurCinq}
-      readonly={true}
-    />
-  )
-}
-
 
 
 export default AfficherAvis
