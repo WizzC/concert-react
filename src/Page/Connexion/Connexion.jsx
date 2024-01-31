129e533a6eb2d9bd2ca9ea1219e7ed7b99308216
WizzC <clementcarlier310@gmail.com>
2024-01-31 10:14:20 +0100

    token et connection

--- src/Page/Connexion/Connexion.jsx
+++ src/Page/Connexion/Connexion.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import Style from './Connexion.module.css'
@@ -8,6 +8,7 @@ import Style from './Connexion.module.css'
 
 
 function Connexion() {
+  let user ={"Email":"","Password":""};
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
 
@@ -15,27 +16,57 @@ function Connexion() {
     setIsOpen(true);
   }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
-
   function closeModal() {
     setIsOpen(false);
   }
+  function connexionRequest(){
+    let tokens;
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    };
+
+    fetch("https://localhost:44314/api/User/authenticate", options)
+        .then(res => res.json())
+        .then(users => {
+            tokens = users.token;
+            console.log(tokens);
+            // test(tokens);  // Appel à test après avoir obtenu le token
+        });
+
+  //   function test(tokens) {
+  //       fetch("https://localhost:44314/api/User", {
+  //           headers: { Authorization: `Bearer ${tokens}` }
+  //       }).then(res => res.json())
+  //       .then(users => { console.log(users) })
+  //       .catch(error => {
+  //           console.error('Fetch error:', error);
+  //       });
+  //  }
+}
+
+   function recupForm(e){
+    e.preventDefault();
+    user["Email"]=e.target[0].value;
+    user["Password"]=e.target[1].value;
+    connexionRequest()
+   }
+
 
   return (
     <div >
       <button className={Style.btnConnexion} onClick={openModal}>Connexion</button>
       <Modal
         isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         contentLabel="Example Modal"
         className={Style.modal}
       >
         <h2>Connexion </h2>
-        <form action="">
+        <form onSubmit={recupForm} method='POST'>
           <div className={Style.divForm}>
             <label htmlFor="mail">Mail</label>
             <input className={Style.inputForm} id="mail" type="text" />
@@ -51,5 +82,6 @@ function Connexion() {
     </div>
   );
 }
+
 
 export default Connexion
